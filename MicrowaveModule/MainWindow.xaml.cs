c7d198351198ec3761d779fd5f1edb46122c72c5
LinKrik <linkrik97@gmail.com>
2020-07-24 16:53:56 +0400

    wrote an interface

---
+++ MicrowaveModule/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MicrowaveModule
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ControlMenuContent.Content = UCControl;
+        }
+
+        UserControlConnect UCConnect = new UserControlConnect();
+        UserControlControl UCControl = new UserControlControl();
+        UserControlMemory UCMemory = new UserControlMemory();
+
+        private void buttonConnectMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ControlMenuContent.Content= UCConnect;
+        }
+
+        private void buttonControlMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ControlMenuContent.Content = UCControl;
+        }
+
+        private void buttonMemoryMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ControlMenuContent.Content = UCMemory;
+        }
+    }
+}
